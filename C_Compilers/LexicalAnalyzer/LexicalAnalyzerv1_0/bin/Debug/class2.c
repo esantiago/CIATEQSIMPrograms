a4e99da4936615abef3fac04bf2c88a9c1a42daa
esantiago <39182318+esantiago@users.noreply.github.com>
2020-06-12 23:04:47 -0500

    Add files via upload

---
+++ C_Compilers/LexicalAnalyzer/LexicalAnalyzerv1_0/bin/Debug/class2.c
@@ -0,0 +1,44 @@
+
+
+ 
+void get_number()
+{
+    int this_is_a_number;
+ 
+    printf( "Please enter a number: " );
+    scanf( "%d", &this_is_a_number );
+    printf( "You entered %d", this_is_a_number );
+}
+
+
+
+struct __sFile 
+{
+  int unused;
+}
+
+
+
+
+
+
+
+int main(){
+    
+
+i++;
+	printf("Hello World!");
+
+	return 0
+
+}
+
+
+int not_main(){
+    printf("This is the value of PI %f", PI);
+
+}
+
+
+
+
